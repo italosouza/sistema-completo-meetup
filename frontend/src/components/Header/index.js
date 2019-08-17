f78d4c58abfa9778d76acbbf0901e2cc7df68bcb
Italo Souza <italoasouza@gmail.com>
2019-08-17 15:35:46 -0300

    add and remove products

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { MdShoppingBasket } from 'react-icons/md'
 import { Container, Cart } from './styles'
 import logo from '../../assets/images/logo.svg'
 
-export default function Header() {
+function Header({ cart }) {
   return (
     <Container>
       <Link to='/'>
@@ -14,10 +15,16 @@ export default function Header() {
       <Cart to='/cart'>
         <div>
           <strong>Meu carrinho</strong>
-          <span>3 itens</span>
+          <span>{cart.length} itens</span>
         </div>
         <MdShoppingBasket size={36} color='#fff' />
       </Cart>
     </Container>
   )
 }
+
+const mapStateToProps = state => ({
+  cart: state.cart,
+})
+
+export default connect(mapStateToProps)(Header)
--- frontend/src/pages/Cart/index.js
+++ frontend/src/pages/Cart/index.js
@@ -1,13 +1,16 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import {
   MdRemoveCircleOutline,
   MdAddCircleOutline,
   MdDelete,
 } from 'react-icons/md'
 
+import PropTypes from 'prop-types'
+
 import { Container, ProductTable, Total } from './styles'
 
-export default function Cart() {
+function Cart({ cart, dispatch }) {
   return (
     <Container>
       <ProductTable>
@@ -21,37 +24,41 @@ export default function Cart() {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>
-              <img
-                src='https://static.netshoes.com.br/produtos/tenis-nike-revolution-4-masculino/06/D12-9119-006/D12-9119-006_detalhe2.jpg?resize=326:*'
-                alt='Tenis'
-              />
-            </td>
-            <td>
-              <strong>Tenis muito massa</strong>
-              <span>R$129,90</span>
-            </td>
-            <td>
-              <div>
-                <button type='button'>
-                  <MdRemoveCircleOutline size={20} color='#7159c1' />
+          {cart.map(product => (
+            <tr>
+              <td>
+                <img src={product.image} alt={product.title} />
+              </td>
+              <td>
+                <strong>{product.title}</strong>
+                <span>{product.priceFormatted}</span>
+              </td>
+              <td>
+                <div>
+                  <button type='button'>
+                    <MdRemoveCircleOutline size={20} color='#7159c1' />
+                  </button>
+                  <input type='number' readOnly value={product.amount} />
+                  <button type='button'>
+                    <MdAddCircleOutline size={20} color='#7159c1' />
+                  </button>
+                </div>
+              </td>
+              <td>
+                <strong>R$258,00</strong>
+              </td>
+              <td>
+                <button
+                  type='button'
+                  onClick={() =>
+                    dispatch({ type: 'REMOVE_FROM_CART', id: product.id })
+                  }
+                >
+                  <MdDelete size={20} color='#7159c1' />
                 </button>
-                <input type='number' readOnly value={2} />
-                <button type='button'>
-                  <MdAddCircleOutline size={20} color='#7159c1' />
-                </button>
-              </div>
-            </td>
-            <td>
-              <strong>R$258,00</strong>
-            </td>
-            <td>
-              <button type='button'>
-                <MdDelete size={20} color='#7159c1' />
-              </button>
-            </td>
-          </tr>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </ProductTable>
 
@@ -65,3 +72,14 @@ export default function Cart() {
     </Container>
   )
 }
+
+Cart.propTypes = {
+  cart: PropTypes.shape().isRequired,
+  dispatch: PropTypes.func.isRequired,
+}
+
+const mapStateToProps = state => ({
+  cart: state.cart,
+})
+
+export default connect(mapStateToProps)(Cart)
