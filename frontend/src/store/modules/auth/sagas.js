9bf99d754d024ca5f6b32492a4ddad201f4b1625
Italo Souza <italoasouza@gmail.com>
2019-08-18 14:50:21 -0300

    login do usuario

--- frontend/src/store/modules/auth/sagas.js
+++ frontend/src/store/modules/auth/sagas.js
@@ -1,16 +1,29 @@
 import { all, takeLatest, put, call } from 'redux-saga/effects'
+
+import { toast } from 'react-toastify'
 import api from '~/services/api'
-import { signInSuccess } from './actions'
+import { signInSuccess, signFailure } from './actions'
 import history from '~/services/history'
 
 export function* signIn({ payload }) {
-  const { email, password } = payload
-  const response = yield call(api.post, `/sessions`, { email, password })
+  try {
+    const { email, password } = payload
+    const response = yield call(api.post, `/sessions`, { email, password })
 
-  const { token, user } = response.data
+    const { token, user } = response.data
 
-  yield put(signInSuccess(token, user))
-  history.push('/dashboard')
+    if (!user.provider) {
+      toast.error('Este usuário não é um prestador de serviços.')
+      return
+    }
+
+    yield put(signInSuccess(token, user))
+    history.push('/dashboard')
+  } catch (err) {
+    toast.error('Falha na autenticação, verifique seus dados.')
+
+    yield put(signFailure())
+  }
 }
 
 export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)])
