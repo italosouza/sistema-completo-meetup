d958a84edf5a1c51b54947c752c1773c32d6991d
Italo Souza <italoasouza@gmail.com>
2019-08-25 13:24:07 -0300

    root import

--- sistemanative/.eslintrc.js
+++ sistemanative/.eslintrc.js
@@ -1,31 +1,43 @@
 module.exports = {
   env: {
     browser: true,
-    es6: true
+    es6: true,
   },
   extends: ['airbnb', 'prettier', 'prettier/react'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
-    __DEV__: 'readonly'
+    __DEV__: true,
   },
   parser: 'babel-eslint',
   parserOptions: {
     ecmaFeatures: {
-      jsx: true
+      jsx: true,
     },
     ecmaVersion: 2018,
-    sourceType: 'module'
+    sourceType: 'module',
   },
-  plugins: ['react', 'prettier'],
+  plugins: ['react', 'jsx-a11y', 'import', 'react-hooks', 'prettier'],
   rules: {
     'prettier/prettier': 'error',
     'react/jsx-filename-extension': [
       'warn',
       {
-        extensions: ['.jsx', '.js']
-      }
+        extensions: ['.jsx', '.js'],
+      },
     ],
+    'import/prefer-default-export': 'off',
+    'no-param-reassign': 'off',
+    'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    'react/jsx-one-expression-per-line': 'off',
+    'global-require': 'off',
+    'react-native/no-raw-text': 'off',
+    'no-underscore-dangle': 'off',
+    'no-console': ['error', { allow: ['tron'] }],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    camelcase: 'off',
+
     'import/prefer-default-export': 'off',
     'react/static-property-placement': [
       'warn',
@@ -35,8 +47,15 @@ module.exports = {
         contextTypes: 'static public field',
         contextType: 'static public field',
         displayName: 'static public field',
-        propTypes: 'static public field'
-      }
-    ]
-  }
+        propTypes: 'static public field',
+      },
+    ],
+  },
+  settings: {
+    'import/resolver': {
+      'babel-plugin-root-import': {
+        rootPathSuffix: 'src',
+      },
+    },
+  },
 }
