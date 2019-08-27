ea4f330147b810e40b3bbf4fe6edcebf202ae60e
Italo Souza <italoasouza@gmail.com>
2019-08-27 19:26:39 -0300

    seleção de horario

--- sistemanative/src/pages/New/SelectDateTime/index.js
+++ sistemanative/src/pages/New/SelectDateTime/index.js
@@ -1,5 +1,6 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
+import PropTypes from 'prop-types'
 import { TouchableOpacity } from 'react-native'
 import Icon from 'react-native-vector-icons/MaterialIcons'
 
@@ -7,15 +8,57 @@ import Background from '~/components/Background'
 // eslint-disable-next-line import/no-unresolved
 import DateInput from '~/components/DateInput'
 
-import { Container } from './styles'
+import { Container, HourList, Hour, Title } from './styles'
+import api from '~/services/api'
 
-export default function SelectDateTime() {
+export default function SelectDateTime({ navigation }) {
   const [date, setDate] = useState(new Date())
+  const [hours, setHours] = useState([])
+
+  const provider = navigation.getParam('provider')
+
+  useEffect(() => {
+    async function loadAvailable() {
+      try {
+        const response = await api.get(`/providers/${provider.id}/available`, {
+          params: {
+            date: date.getTime(),
+          },
+        })
+
+        setHours(response.data)
+      } catch (err) {
+        console.tron.log(err)
+      }
+    }
+    loadAvailable()
+  }, [date, provider.id])
+
+  function handleSelectValue(time) {
+    navigation.navigate('Confirm', {
+      provider,
+      time,
+    })
+  }
 
   return (
     <Background>
       <Container>
         <DateInput date={date} onChange={setDate} />
+
+        <HourList
+          data={hours}
+          extraData={date}
+          keyExtractor={item => item.time}
+          renderItem={({ item }) => (
+            <Hour
+              onPress={() => handleSelectValue(item.value)}
+              enabled={item.available}
+            >
+              <Title>{item.time}</Title>
+            </Hour>
+          )}
+        />
       </Container>
     </Background>
   )
@@ -33,3 +76,10 @@ SelectDateTime.navigationOptions = ({ navigation }) => ({
     </TouchableOpacity>
   ),
 })
+
+SelectDateTime.propTypes = {
+  navigation: PropTypes.shape({
+    getParam: PropTypes.func,
+    navigate: PropTypes.func,
+  }).isRequired,
+}
--- sistemanative/src/services/api.js
+++ sistemanative/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://104.223.143.72:3000',
+  // baseURL: 'http://104.223.143.72:3000',
+  baseURL: 'http://10.0.2.2:3333',
 })
 
 export default api
